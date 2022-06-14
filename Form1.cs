041a8af0ea230e30c8321986eb15b1476da1dbe9
petersm6537 <maverickpetersdesign@gmail.com>
2022-06-14 16:00:01 -0500

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace XMLConfig
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            XMLEntry();
+        }
+
+        static void XMLEntry()
+        {
+            XDocument configXML = new XDocument(
+    new XComment("This will be my config file for the mold"),
+    new XElement("swConfig",
+        new XElement("MoldID", "1-123"),
+        new XElement("Revision", "PRA1"),
+        new XElement("ID", ".625"),
+        new XElement("XS", ".075")
+    ));
+
+            configXML.Save(@"C:\Users\peter\desktop\configXML");
+
+        }
+    }
+}
